d76c691103c65e3f595ec8cd6bad69d142b36da5
Joergen Ibsen <ji@ibse.dk>
2023-11-01 08:49:26 +0100

    Add SPDX license identifiers

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -15,6 +15,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+// SPDX-License-Identifier: Apache-2.0
+//
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
